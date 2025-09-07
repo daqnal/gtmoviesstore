96983dcc1638291548484c4e45d1430abc46ad1f
daqnal <dxgd@pm.me>
2025-09-07 13:49:25 -0400

    final tweaks

--- home/views.py
+++ home/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from movies.models import Movie
+
 
 def index(request):
+    movies = Movie.objects.all()[:3]
     template_data = {}
-    template_data['title'] = 'GT Movies Store'
-    return render(request, 'home/index.html', {'template_data': template_data})
+    template_data["title"] = "GT Movies Store"
+    template_data["movies"] = movies
+    return render(request, "home/index.html", {"template_data": template_data})
+
 
 def about(request):
     template_data = {}
-    template_data['title'] = 'About'
-    return render(request, 'home/about.html', {'template_data': template_data})
+    template_data["title"] = "About"
+    return render(request, "home/about.html", {"template_data": template_data})
